a9b78763cc3c1ea958cbd6f367ce31d9f3401c44
SendriAska <thomas.boscaro@gmail.com>
2025-08-27 14:27:30 +0200

    HomeController + View

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//imposter les ressources
+include "./env.php";
+include "./vendor/autoload.php";
+
+//Analyse de l'URL avec parse_url() et retourne ses composants
+$url = parse_url($_SERVER['REQUEST_URI']);
+//test si l'url posséde une route sinon on renvoi à la racine
+$path = $url['path'] ??  '/';
+
+session_start();
+
+// import des classes router
+use App\Controller\HomeController;
+
+// instanciation du contrôleur
+$homeController = new HomeController();
+
+// Test route
+switch (substr($path, strlen(BASE_URL))) {
+    case "/":
+        $homeController->home();
+        break;
+    case "/user/inscription":
+        break;
+}
